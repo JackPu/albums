ecce46103a355eed14a5a431c529a05ad60fda19
JackPu <kakashjack@gmail.com>
2015-10-18 23:23:44 +0800

    fix  path error

---
+++ projects/release.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Only for my demo. It's not important!
+sudo cp -rf /var/www/albums/www/resource /var/www/design/www/
+sudo cp -rf /var/www/albums/App/Lib /var/www/design/App/
+sudo cp -rf /var/www/albums/App/View /var/www/design/App/
