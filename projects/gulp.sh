815b227f19c665b5136982d1df0d0372c4e11cc3
JackPu <kakashjack@gmail.com>
2015-11-01 16:39:54 +0800

    add gulp.sh

---
+++ projects/gulp.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+### gulp 初始化
+echo 'init gulp'
+npm install gulp --save-dev
+npm install gulp-uglify --save-dev
+npm install gulp-plumber --save-dev
+npm install gulp-less --save-dev
+npm install gulp-minify-css --save-dev
+npm install gulp-rename --save-dev
+npm install gulp-util --save-dev
+echo 'gulp 模块安装完毕'
