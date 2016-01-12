cf9215037304c63debb629098ac0871f385c7f88
JackPu <kakashjack@gmail.com>
2016-01-12 23:10:46 +0800

    add user/profile

--- www/static/js/app.js
+++ www/static/js/app.js
@@ -19,9 +19,6 @@ define(['angularAMD', 'angular-route','directive/pagination'], function (angular
         templateUrl: 'home/upload/view', controller: 'uploadCtrl', controllerUrl: ANGULAR_CTRL_PATH + 'upload.js'
     }))
     
-    .when("/upload", angularAMD.route({
-        templateUrl: 'home/upload/view', controller: 'uploadCtrl', controllerUrl: ANGULAR_CTRL_PATH + 'upload.js'
-    }))
     
     .when("/user/login", angularAMD.route({
         templateUrl: 'user/login/view', controller: 'LoginCtrl', controllerUrl: ANGULAR_CTRL_PATH + 'user/login.js'
@@ -31,9 +28,7 @@ define(['angularAMD', 'angular-route','directive/pagination'], function (angular
         templateUrl: 'user/register/view', controller: 'RegisterCtrl', controllerUrl: ANGULAR_CTRL_PATH + 'user/register.js'
     }))
     
-    .when("/upload", angularAMD.route({
-        templateUrl: 'home/upload/view', controller: 'uploadCtrl', controllerUrl: ANGULAR_CTRL_PATH + 'upload.js'
-    }))
+
     
     .otherwise({redirectTo: "/home"});
      //$locationProvider.html5Mode(true);   
