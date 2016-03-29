8c2578b2fd1ad778f8ceccf346ee5c426414ae92
JackPu <kakashjack@gmail.com>
2016-03-29 12:05:56 +0800

    add new release.sh

--- projects/release.sh
+++ projects/release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Only for my demo. It's not important!
-sudo cp -rf /var/www/albums/www/resource /var/www/design/www/
+sudo cp -rf /var/www/albums/www/static /var/www/design/www/static
 sudo cp -rf /var/www/albums/App/ /var/www/design/App/
 sudo cp -rf /var/www/albums/view /var/www/design/view
