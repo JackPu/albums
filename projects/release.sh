b1fe00cc756ce3ab40ab1af575eb2cb0e7ceb74b
JackPu <kakashjack@gmail.com>
2016-08-05 22:19:26 +0800

    update release shell

--- projects/release.sh
+++ projects/release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Only for my demo. It's not important!
-sudo cp -rf /var/www/albums/www/static/* /var/www/design/www/static/
-sudo cp -rf /var/www/albums/App/* /var/www/design/App/
-sudo cp -rf /var/www/albums/view/* /var/www/design/view/
+sudo cp -rf ./albums/src ./www.jackzoo.org/
+sudo cp -rf ./albums/www/static ./www.jackzoo.org/www
+sudo cp -rf ./albums/view ./www.jackzoo.org/
